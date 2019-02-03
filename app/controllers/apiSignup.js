98b75fdae78f394d84cca87d44e93e541f68022c
Ivan <guerrero.ivan.789@gmail.com>
2019-02-02 22:23:05 -0500

    fix

--- app/controllers/apiSignup.js
+++ app/controllers/apiSignup.js
@@ -8,11 +8,11 @@ var apiSignup  = {
 }
 
 function apiSignup(req, res){
-    connection.query("SELECT * FROM usuarios WHERE username = ?",[username], function(err, rows){
+    connection.query("SELECT * FROM usuarios WHERE username = ?",[req.body.username], function(err, rows){
         if (err)
             return done(err);
         if (!rows.length) {
-            connection.query("INSERT INTO usuarios (username, password, user_email) VALUES (?,?,?)",[username, password, email], function(err2, rows2){
+            connection.query("INSERT INTO usuarios (username, password, user_email) VALUES (?,?,?)",[req.body.username, req.body.password, req.body.email], function(err2, rows2){
                 if(err2) return done(err2);
                 if(rows2.length){
                     res.json({ "registrado": true });
