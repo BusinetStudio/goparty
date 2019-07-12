e3b7f8fd5621e7a0f13e42581e7c931f543342b7
Ivan <guerrero.ivan.789@gmail.com>
2019-07-12 14:40:42 -0500

    12.7.2019.14.40

--- app/routes/api/users.js
+++ app/routes/api/users.js
@@ -43,7 +43,7 @@ router.post('/users/login', function(req, res, next){
   passport.authenticate('app', {
     session: false,
   }, function(err, user, info){
-    if(err || !user){ return res.json({success : false, error: err}); } 
+    if(err || !user){ return res.json({success : false, error: info}); } 
     user.token = user.generateJWT();
     return res.json({success : true, user: user.toAuthJSON()});
   })(req, res, next);
