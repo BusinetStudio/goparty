df6ba372fd4c54513d3bcf21c8caf2fe8de60720
Ivan Guerrero <guerrero.ivan.789@gmai.com>
2019-11-04 02:16:14 -0500

    0.11.4.19.2.16.0

--- app/routes/api/users.js
+++ app/routes/api/users.js
@@ -62,11 +62,14 @@ router.post('/users/register', async function(req, res, next){
 
 router.post('/users/usuarioProfile', async function(req, res, next){
   var resultado;
-  User.findOne({_id: req.body.id_usuario}).then(usuario=>{
-    resultado = usuario;
-    return UsuariosInfo.findOne({id_usuario: req.body.id_usuario})
-  }).then(profile=>{
-    return res.json({valid:true, result: {...profile, ...resultado}})
+  User.findOne({_id: req.body.id_usuario}, (err,usuario)=>{
+    if(usuario){
+      UsuariosInfo.findOne({id_usuario: req.body.id_usuario},(err, profile)=>{
+        if(profile){
+          return res.json({valid:true, result: {...profile, ...usuario}})
+        }
+      })
+    }
   })
 });
 router.post('/users/usuarioProfileUpdate', function(req, res, next){
