2f9d94f32826f17fc2b88c566265e750dd6506cb
Ivan Guerrero <guerrero.ivan.789@gmai.com>
2019-11-04 23:16:18 -0500

    0.11.4.19.11.16.1

--- app/routes/api/cotizaciones.js
+++ app/routes/api/cotizaciones.js
@@ -71,12 +71,12 @@ router.post('/borrarCotizacion', function(req, res, next){
     return res.json({valid:false})
 }); 
 router.post('/CotizacionesOrderByCategoria', function(req, res, next){
-    Cotizaciones.find({id_evento: req.body.id_evento}, async function (err,result) {
+    Cotizaciones.find({id_evento: req.body.id_evento}, function (err,result) {
         if(err) return res.json({valid:false});
         else {
             var cotizaciones = result;
             var resultado=[]
-            cotizaciones.forEach(r=>{
+            cotizaciones.forEach(async r=>{
                 var profile = await ProveedoresInfo.findOne({id_proveedor: r.id_proveedor});
                 for(key in r.cotizacion[req.body.categoria]){
                     resultado[profile.nombreEmpresa]=r.cotizacion[req.body.categoria]
